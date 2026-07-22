9a7608bf047fe9a20086b45af98b5ae8f10f21cc
lifeng6666 <274837845+lifeng6666@users.noreply.github.com>
2026-07-22 16:52:26 +0800

    Update summer01.py

--- summer01.py
+++ summer01.py
@@ -205,6 +205,7 @@ def create_chrome_driver(user_data_dir=None, proxy_str=None):
         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     )
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--blink-settings=imagesEnabled=false")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_argument("--no-sandbox")
